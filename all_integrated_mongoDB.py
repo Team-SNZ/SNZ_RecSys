666ddb2dbf7299f8b9550b4a23bf66a8d9b732bd
Jaeyoon Shim <sjy21ys@yonsei.ac.kr>
2025-08-08 17:56:15 +0900

    Feat: class확정

--- all_integrated_mongoDB.py
+++ all_integrated_mongoDB.py
@@ -52,11 +52,10 @@ Multi-agent: MyState, Chatbot agent, Profiler agent, Recommender agent, sperviso
 """
 class MyState(TypedDict, total=False):
     user_id: int
-    messages: List[Dict[str, str]]            # {"role": "user|assistant", "content": "..."}
     profile: str                               # user 요약 정보
-    rec_people: List[str]                      # 매칭 후보 id
-    rec_travel: List[str]                      # 추천 여행지 id
-    summary: str                               # Q&A 요약
+    rec_people: List[int]                      # 매칭 후보 id
+    rec_travel: List[str]                      # 추천 여행지 link
+    top_100_ids: List[int]                  
 
 llm = ChatOpenAI(model="gpt-4o", temperature=0.3)
 
